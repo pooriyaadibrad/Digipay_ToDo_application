a0ebdf5ca73267a2db76faffec92747110dec5e6
pooriya <adibradp.enginear@gmail.com>
2025-01-24 23:02:33 +0330

    the todo details url be added

--- todo_application/todo/urls.py
+++ todo_application/todo/urls.py
@@ -4,4 +4,5 @@ from todo import views
 
 urlpatterns = [
     path('create_list/', views.TodoListCreateApiView.as_view(), name='create_list'),
+    path('update_delete_retrieve/<str:pk>',views.TodoDetailsApiView.as_view(), name='update_delete_retrieve')
 ]
