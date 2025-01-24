c74a1da8cbca5cbb53d0d79c93b1369c83c720bd
pooriya <adibradp.enginear@gmail.com>
2025-01-24 21:30:49 +0330

    the ToDo model be created

--- todo_application/todo/models.py
+++ todo_application/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django_jalali.db import models as jalali_models
 
-# Create your models here.
+
+class Todo(models.Model):
+    """
+    this model use for stored _todo_ information in database
+    """
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = jalali_models.jDateTimeField()
+    completed = models.BooleanField(default=False)
+    created_at = jalali_models.jDateTimeField(auto_now_add=True)
+    updated_at = jalali_models.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'ToDO_list'
